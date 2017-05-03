7ce1bca7537b14f44c9ca599a7c1a3481e8b431b
OOC <whatisooc@gmail.com>
2017-05-03 22:26:05 +0800

    add SimpleAdapter

--- app/src/main/java/com/ooczc/spammess/MainActivity.java
+++ app/src/main/java/com/ooczc/spammess/MainActivity.java
@@ -3,35 +3,121 @@ package com.ooczc.spammess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
     Button bt;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除标题栏
+        setContentView(R.layout.listview);
         //通过int型反射，获取控件对象
         //这里的tv_a1必须在setContentView(R.layout.activity_main)的activity_main中.
+
+        ListView listView = (ListView) findViewById(R.id.lv_main);
+
+
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("num","234333124");
+        map.put("mess","大幅上升1");
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","234000004");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","3143124");
+        map.put("mess","大幅上升");
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","333143124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","23242443124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","1111143124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+        map = new HashMap<String,Object>();
+        map.put("num","234000004");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","3143124");
+        map.put("mess","大幅上升");
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","333143124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","23242443124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        map = new HashMap<String,Object>();
+        map.put("num","1111143124");
+        map.put("mess",getResources().getString(R.string.mess));
+        list.add(map);
+
+        SimpleAdapter adapter = new SimpleAdapter(this,list,R.layout.item,
+                new String[]{"num","mess"},new int[]{R.id.tv_num,R.id.tv_mess}
+                );
+        listView.setAdapter(adapter);
+
+
+//        String[] data = {"大大","大法师","更改","个地方","(･ｪ-)","哈根","统一","发改","的点点滴滴"
+//                ,"大大","大法师","更改","个地方","(･ｪ-)","哈根","统一","发改","的点点滴滴"};
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//                this,android.R.layout.simple_list_item_1,data
+//        );
+//        listView.setAdapter(adapter);
+        /*
+        et = (EditText) findViewById(R.id.et_1);
 
         //给Button设置监听器
         bt = (Button) findViewById(R.id.button1);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"很好",Toast.LENGTH_LONG).show();
+                String text = et.getText().toString();
+                Toast.makeText(MainActivity.this,text,Toast.LENGTH_LONG).show();
                 //第一个参数为MainActicity对象引用,要求为上下文类型（即Activity）
-
 
             }
         });
 
 
-
-
-        /*
         TextView tv = (TextView) findViewById(R.id.tv_a1);
         //
         String text = (String) tv.getText();
